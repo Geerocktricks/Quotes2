99fbceab98977b40bf7477ccbeda5b47af7d898d
Geerocktricks <geerockface4@gmail.com>
2018-11-15 15:10:34 +0300

    initial commit

--- gitsearcher2/src/app/app.module.ts
+++ gitsearcher2/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LalndingPageComponent } from './lalnding-page/lalnding-page.component';
-import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LalndingPageComponent,
-    FormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
